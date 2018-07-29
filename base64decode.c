d758ba1ed45d3ad60d4f47f157be8af8c9434186
Elliot Friedman <efriedma@e1z3r1p22.42.us.org>
2018-07-28 22:51:00 -0700

    Framework for b64 decode have been created

--- base64decode.c
+++ base64decode.c
@@ -6,89 +6,86 @@
 /*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 13:29:03 by efriedma          #+#    #+#             */
-/*   Updated: 2018/07/09 13:58:29 by efriedma         ###   ########.fr       */
+/*   Updated: 2018/07/28 22:50:36 by efriedma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "openssl.h"
 
-				   //0123456789012345678901234567890123456789012345678901234567890123
-char    g_ref[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const char	g_ref[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+int			g_idx[127];// = {['A'] = 0, ['B'] = 1, ['C'] = 2, ['D'] = 3, ['E'] = 4, ['F'] = 5};
+int			g_pad;
 
 int	findb_len(int len)
 {
 	int	t_bits;
 
-	t_bits = len * 6;
-	while (t_bits % 8 != 0)
-		t_bits--;
-	return (t_bits / 8);
+	t_bits = len * 8;
+	while (t_bits % 6 != 0)
+		t_bits++;
+	return (t_bits / 6);
+}
+
+void			init_idx(void)
+{
+	size_t		i;
+	char		a;
+
+	i = -1;
+	a = 'A';
+	while (++i < 26 && a++)
+	{
+		g_idx[a] = i;
+	}
+	a = 'a';
+	while (++i < 52 && a++)
+	{
+		g_idx[a] = i;
+	}
+	a = '0'
+	while (++i < 62 && a++)
+	{
+		g_idx[a] = i;
+	}
+	g_idx['+'] = i++;
+	g_idx['/'] = i++;
+	//g_idx['='] does not need to be set equal to 0 because globals are zeroed by default
+
 }
 
 unsigned char	*base64_decode(unsigned char *str, int len)
 {
-	int		nlen;
 	int		bit_len;
 	int		m;
 	size_t	i;
-	size_t	b00l;
 	unsigned char	*n;
 
-	/*
-	 *
-	 *		Make a boolean to see if we need = sign or not
-	 *	this is very important for padding if we need it
-	 *	
-	 *	make a base 64 decode too
-	 *
-	 *	add ciper error messages and b64, add these as functions :)
-	 *
-	 */
-	b00l = (len * 8) % 6;
+	g_pad = ((len * 8) % 3);
 	bit_len = findb_len(len);
-	n = ft_memalloc(bit_len);
+	n = ft_memalloc(bit_len + 1);
 	i = 0;
 	m = 0;
-	while (i < bit_len)
+	//ft_printf("bit_len %d\n", bit_len);
+	//
+	//turn 4bytes to 
+	//
+	while ((int)i < bit_len)
 	{
-		//int z = magico(&m, str);
-		//1			
-		//		    \/	   \/
-		//			0000  0000 | 0000  0000 | 0000 0000 
-			n[i] = g_ref[(str[m] & 252) >> 2];//magico(m, str)];
-		//2				   \/	     \/ 	  
-		//			0000  0000 | 0000  0000 | 0000 0000 
-			n[i + 1] = g_ref[(  ((str[m] & 3) << 4) |  ((str[m + 1] & 240) >> 4))];
-		//3
-		//							 \/		   \/
-		//			0000  0000 | 0000  0000 | 0000 0000
-		//						   11  1111
-			n[i + 2] = g_ref[(  ((str[m + 1] & 15) << 2)   |   (((str[m + 2] & 192) >> 6))   )];
-		//4
-		//									   \/	  \/
-		//			0000  0000 | 0000  0000 | 0000 0000
-			n[i + 3] = g_ref[(     str[m + 2] & 63  )];
-		//ft_printf("m: %d, i: %d\n", m, i);
+		n[i] = g_ref[(str[m] & 252) >> 2];
+		n[i + 1] = g_ref[(  ((str[m] & 3) << 4) |  ((str[m + 1] & 240) >> 4))];
+		n[i + 2] = g_ref[(  ((str[m + 1] & 15) << 2)   |   (((str[m + 2] & 192) >> 6))   )];
+		n[i + 3] = g_ref[(     str[m + 2] & 63  )];
 		m += 3;
 		i += 4;
 	}
-	while (b00l)
+	//ft_printf("n before padding %s\n", n);
+	i -= g_pad;
+	while (g_pad)
 	{
-		i -= 1;
+		//ft_printf("appending =\n");
 		n[i] = '=';
 		i++;
-		b00l--;
+		g_pad--;
 	}
 	return (n);
-}
-
-int main(int argc, char **argv)
-{
-	if (argc == 2)
-	{
-		unsigned char *n = base64_encode((unsigned char *)argv[1], ft_strlen((char*)argv[1]));
-		ft_printf("%s\n", n);
-		free(n);
-	}
-	return (0);
 }
