aaa6173d57d289162712c6e1fd81ef7d80c6daaa
Elliot Friedman <efriedma@e1z2r3p6.42.us.org>
2018-08-02 14:16:07 -0700

    Cleaned up comments, removed garbage files

---
+++ commands.sh
@@ -0,0 +1,43 @@
+#password 
+
+openssl enc -des -pass pass:MYPASSWORD -P -S 2F6760E3D7713CBA
+
+# -A                 Process base64 data on one line (requires -a)
+# -a                 Perform base64 encoding/decoding (alias -base64)
+# -bufsize size      Specify the buffer size to use for I/O
+# -d                 Decrypt the input data
+# -debug             Print debugging information
+# -e                 Encrypt the input data (default)
+# -engine id         Use the engine specified by the given identifier
+# -in file           Input file to read from (default stdin)
+# -iv IV             IV to use, specified as a hexidecimal string
+# -K key             Key to use, specified as a hexidecimal string
+# -md digest         Digest to use to create a key from the passphrase
+# -none              Use NULL cipher (no encryption or decryption)
+# -nopad             Disable standard block padding
+# -out file          Output file to write to (default stdout)
+# -P                 Print out the salt, key and IV used, then exit
+#                      (no encryption or decryption is performed)
+#
+# -p                 Print out the salt, key and IV used
+# -pass source       Password source
+# -S salt            Salt to use, specified as a hexidecimal string
+# -salt              Use a salt in the key derivation routines (default)
+# -v                 Verbose
+
+
+#		Flags I Have to be Concerned About
+
+#
+# -a, decode/encode the input/output in base64, depending on the encrypt mode
+# -d, decrypt mode
+# -e, encrypt mode (default)
+# -i, input file for message
+# -k, key in hex is the next arguement.
+#	(Behave like openssl des -K not openssl des -k)
+#
+# -o, output file for message
+# -p, password in ascii is the next argument.
+#(Behave like a modifiedd openssl des -pass not like openssl des -p or -P) (A verbose explanation is given in the next section)
+# -s, the salt in hex is the next argument. (Behave like openssl des -S)
+# -v, initialization vector in hex is the next argument. (Behave like openssl des -iv not openssl des -v)
--- des/des.c
+++ des/des.c
@@ -6,39 +6,31 @@
 /*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/01 16:06:46 by efriedma          #+#    #+#             */
-/*   Updated: 2018/08/01 18:19:52 by efriedma         ###   ########.fr       */
+/*   Updated: 2018/08/02 14:15:39 by efriedma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../openssl.h"
 
-FILE			*e;	
 
-int			_rand0(void)
+int			_rand0(FILE *e)
 {
 	unsigned int	a;
 
-	a = 0;
-	if ((e = fopen("/dev/urandom", "r")))
-	{
-		a = (unsigned int)getc(e);
-		while ((a & 127) < 32)
-		{
-			a += (unsigned int)getc(e);
-		}
-	}
+	a = (unsigned int)getc(e);
+	while ((a & 127) < 32)
+		a += (unsigned int)getc(e);
 	return ((int)a);
 }
 
-void		create_salt_8bytes(char *salt)
+void		create_salt_8bytes(char *salt, FILE *e)
 {
-	//use /dev/random
 	int		i;
 
 	i = 0;
 	while (i < 8)
 	{
-		salt[i] = (char)_rand0() & 127;
+		salt[i] = (char)_rand0(e) & 127;
 		i++;
 	}
 	salt[8] = 0;
@@ -47,7 +39,7 @@ void		create_salt_8bytes(char *salt)
 void		swap_4bytes(int *data)
 {
 	int	a;
-	
+
 	a = data[0];
 
 	data[0] = data[1];
@@ -63,9 +55,7 @@ void		print_bytes(void *data, int len)
 	swap_4bytes((int*)data);
 	while (i < len)
 	{
-		//ft_printf("%d\n", print[i]);
 		ft_printf("%02X", print[i]);
-		//ft_printf("i: %d\n", i);
 		i++;
 	}
 	ft_putstr("\n");
@@ -77,39 +67,24 @@ void		des(char **argv, int argc)
 	unsigned long long	*tmp;
 	unsigned long long	*key;
 	char 				*pass;
-	char				salt[9];// = malloc(9);
-	int		i = 0;
+	char				salt[9];
+	int					i = 0;
+	FILE            	*e;
 
 	pass = getpass("Enter your password:");
-	//parse for all options to decide how to handle
-
-	//have user enter their password
-	//
-	//store in pass
-	//
-	//salt is created
-	//
-	//salt is concatenated on pass
-	//while (i < 30)
-	//{
-
-	create_salt_8bytes(salt);
+	e = fopen("/dev/urandom", "r");
+	create_salt_8bytes(salt, e);
 	ft_printf("Random salt: %s\n", salt);
 	fclose(e);
 	ft_printf("Salt: %02X%02X%02X%02X%02X%02X%02X%02X\n", salt[0], salt[1], salt[2], salt[3], salt[4], salt[5],salt[6], salt[7]);
-	//	i++;
-	//}
 
-	//for some reason getpass allocates on the stack so you can't free. Hmmm
-	//pass = ft_strjoin(pass, salt);
-	ft_printf("pass: %s\n", pass);
 	tmp = create_key(pass);
 	key = tmp;
-	ft_putstr("key=");
+	ft_putstr("key = ");
 	print_bytes((void*)tmp, 8);
+	ft_printf("key in binary: %064b\n", key);
 	tmp++;
 	ft_putstr("iv =");
 	print_bytes((void*)tmp, 8);
 
-	//free(salt);
 }
