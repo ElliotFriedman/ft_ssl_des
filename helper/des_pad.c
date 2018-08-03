17b04dc11e5b0897cbafa485a93992cbaa282c8f
Elliot Friedman <efriedma@e1z2r3p10.42.us.org>
2018-08-02 19:34:04 -0700

    byte pad created

--- helper/des_pad.c
+++ helper/des_pad.c
@@ -6,44 +6,56 @@
 /*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/02 18:02:38 by efriedma          #+#    #+#             */
-/*   Updated: 2018/08/02 18:47:16 by efriedma         ###   ########.fr       */
+/*   Updated: 2018/08/02 19:33:48 by efriedma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../openssl.h"
+
+void    print_bytes(char *str, int len);
 
 char		*add_byte(char *data, size_t bytes)
 {
 	char	*snew;
 	char	*tmp;
 
+	//this will be freed after we re assign data
 	tmp = data;
 	bytes++;
+	//add an extra byte
 	snew = ft_memalloc(bytes);
+	//copy the old bytes in from data
 	ft_memcpy(snew, data, bytes - 1);
+	//free the old data
 	free(tmp);
+	//return the new data
 	return (snew);
 }
 
-int		des_pad(char **encrypt, size_t len)
+char	*des_pad(char *encrypt, size_t len)
 {
 	size_t	dif;
-	char	*set;
+	char	set[1];
 
 	dif = len % 8;
 	ft_printf("dif: %d\n", dif);
-	*set = 8 - dif;
+	set[0] = 8 - dif;
 	if (dif == 0)
-		*set = 8;
-	dif = *set;
+		set[0] = 8;
+	dif = set[0];
+	ft_printf("adding %d bytes\n", dif);
 	while (dif)
 	{
+		ft_printf("Dif in while loop %d, Len: %d\n", dif, len + 1);
 		len++;
-		*encrypt = add_byte(*encrypt, len);
-		ft_memset(&*encrypt[len], set, 1);
+		encrypt = add_byte(encrypt, len);
+
+		ft_memset(&encrypt[len - 1], (int)set[0], 1);
+		print_bytes(encrypt, len);
 		dif--;
 	}
-	return (len);
+	ft_printf("total len %d\n", len);
+	return (encrypt);
 }
 
 void	print_bytes(char *str, int len)
@@ -60,11 +72,11 @@ void	print_bytes(char *str, int len)
 
 int main(void)
 {
-	char *a = malloc(6);
-	ft_strcpy(a, "Pad me");
-	print_bytes(a, 6);
-	des_pad(&a, 6);
-	print_bytes(a, 8);
+	char *a = malloc(8);
+	ft_strcpy(a, "Pad me12");
+	print_bytes(a, ft_strlen(a));
+	a = des_pad(a, ft_strlen(a));
+	print_bytes(a, 19);
 
 	return 1;
 }
