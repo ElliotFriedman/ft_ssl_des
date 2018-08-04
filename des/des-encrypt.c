9bf69159b5431af9f240ac328b26c4654693ff02
Elliot Friedman <efriedma@e1z2r4p11.42.us.org>
2018-08-03 20:58:57 -0700

    added expansion permutation constants

--- des/des-encrypt.c
+++ des/des-encrypt.c
@@ -6,7 +6,7 @@
 /*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/03 18:20:16 by efriedma          #+#    #+#             */
-/*   Updated: 2018/08/03 20:44:34 by efriedma         ###   ########.fr       */
+/*   Updated: 2018/08/03 20:58:41 by efriedma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,18 @@
 //Then check logic errors
 //Then check memory issues
 
+
+//compression permutation
 int		g_cpermutation[48]={14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
 							23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
 							41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
 							44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32};
+
+//expansion permutation
+int		g_expandpermutation[48] = {32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9,
+									8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
+									16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
+									24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1};
 
 int		g_rotate[16];
 
@@ -45,21 +53,38 @@ void	create_subkey(unsigned long long key, size_t *sub_key)
 	sub_key[1] = (size_t)(key >> 8);
 }
 
+void	expansion_permutation(unsigned long long *bside)
+{
+	size_t	i;
 
+	i = 0;
+
+
+
+
+}
 
 char	*encrypted_des(char *data, unsigned long long key, size_t *sub_key)
 {
-	size_t	i;
-	int		aside;
-	int		bside;
+	size_t				i;
+	unsigned long long	aside;
+	unsigned long long	bside;
 
 	i = 0;
+	aside = 0;
+	bside = 0;
 	//break data into 2 4 byte blocks
 	ft_strncpy((char*)aside, data, 4);
 	ft_strncpy((char*)bside, data, 4);
+	//shift b right 32 times so that bytes are in order
+	bside >>= 32;
 	//do a round of 16, and return the result
 	while (i < 16)
 	{
+		//run expansion permutation on bside
+		
+	
+
 		//rotate subkeys each round
 		//This is wrong needs to be Compressed,
 		//sub_key[0] = LROT(sub_key[0], g_rotate[i]);
