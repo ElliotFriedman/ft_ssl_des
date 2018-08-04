e6bd8db4081ac6a618a82dcb4e6465990686880d
Elliot Friedman <efriedma@e1z2r4p11.42.us.org>
2018-08-03 21:42:07 -0700

    Added explanation to endian tester

--- little_big_endian.c
+++ little_big_endian.c
@@ -6,7 +6,7 @@
 /*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/02 14:38:10 by efriedma          #+#    #+#             */
-/*   Updated: 2018/08/02 14:52:05 by efriedma         ###   ########.fr       */
+/*   Updated: 2018/08/03 21:41:57 by efriedma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,12 @@ int		main(void)
 	//Little Endian
 	//0000 0001.... 0000 0000
 
+	//if we are big endian, the first byte will be 0's
 	if(*a == 0)
 		printf("You are running a Big Endian machine\n");
 	else
 		printf("You are running a Little Endian machine\n");
+	//if we are little endian, the first byte will be 1
 
 	return (0);
 }
